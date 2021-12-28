03ae151a7df491d0041030e501e8f42f44d9a540
KIMJIMIN2 <jimin101477@naver.com>
2021-12-28 17:01:19 +0900

    2021-12-28

---
+++ ioc/src/com/company/ioc/Encoder.java
@@ -0,0 +1,15 @@
+package com.company.ioc;
+
+import java.util.Base64;
+
+public class Encoder {
+    private IEncoder iEncoder;
+
+    public Encoder(IEncoder iEncoder) {
+        this.iEncoder = iEncoder;
+    }
+
+    public String encode(String message) {
+        return Base64.getEncoder().encodeToString(message.getBytes());
+    }
+}
