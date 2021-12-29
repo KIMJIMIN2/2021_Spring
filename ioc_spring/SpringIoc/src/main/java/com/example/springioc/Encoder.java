935a21e8fdc31c3a026f13b8374b9aa363765fa7
KIMJIMIN2 <jimin101477@naver.com>
2021-12-29 16:08:25 +0900

    2021-12-29

--- ioc_spring/SpringIoc/src/main/java/com/example/springioc/Encoder.java
+++ ioc_spring/SpringIoc/src/main/java/com/example/springioc/Encoder.java
@@ -1,12 +1,18 @@
 package com.example.springioc;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+
 import java.util.Base64;
 
 public class Encoder {
     private IEncoder iEncoder;
 
-    public Encoder(IEncoder iEncoder) {
+    public Encoder(@Qualifier("base74Enocder") IEncoder iEncoder) {
         this.iEncoder = iEncoder;
+    }
+
+    public void setIEncoder(IEncoder iEncoder){
+        this.iEncoder=iEncoder;
     }
 
     public String encode(String message) {
