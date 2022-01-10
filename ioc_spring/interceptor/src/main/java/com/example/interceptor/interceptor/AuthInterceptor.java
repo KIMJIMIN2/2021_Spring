03f0521740de485cdd0037bddb8ec782640eefb3
KIMJIMIN2 <jimin101477@naver.com>
2022-01-10 16:00:48 +0900

    2022-01-10

--- ioc_spring/interceptor/src/main/java/com/example/interceptor/interceptor/AuthInterceptor.java
+++ ioc_spring/interceptor/src/main/java/com/example/interceptor/interceptor/AuthInterceptor.java
@@ -1,13 +1,18 @@
 package com.example.interceptor.interceptor;
 
+import com.example.interceptor.annotation.Auth;
+import com.example.interceptor.exception.AuthException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
 
 @Slf4j
 @Component
@@ -17,9 +22,28 @@ public class AuthInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String url = request.getRequestURI();
 
-        log.info("request url : {}",url);
+        URI uri = UriComponentsBuilder.fromUriString(request.getRequestURI())
+                .query(request.getQueryString())
+                .build()
+                .toUri();
 
-        return false;
+        log.info("request url : {}", url);
+        boolean hasAnnotation = checkAnnotation(handler, Auth.class);
+        log.info("has annotation: {}", hasAnnotation);
+
+        // 나의 서버는 모두 public으로 동작을 하는데
+        // 단, Auth 권한을 가진 요청에 대해서는 세션, 쿠키
+        if (hasAnnotation) {
+            // 권한체크
+            String query = uri.getQuery();
+            log.info("query :{}",query);
+            if(query.equals("name=test")){
+                return true;
+            }
+            throw new AuthException();
+        }
+
+        return true;
     }
 
     private boolean checkAnnotation(Object handler, Class clazz) {
